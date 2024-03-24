e76358fcd57fb175f816d1c6f1d9ce296159784d
pleaseppu <94917437+pleaseppu@users.noreply.github.com>
2024-03-24 14:27:54 +0800

    main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,6 +1,6 @@
 const test = require('node:test');
 const assert = require('assert');
 const { Application, MailSystem } = require('./main');
-
+4111
 // TODO: write your tests here
 // Remember to use Stub, Mock, and Spy when necessary
