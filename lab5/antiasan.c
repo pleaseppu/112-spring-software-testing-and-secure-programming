74b18abdb9e04f409ca0339860e0809070c15a7f
pleaseppu <94917437+pleaseppu@users.noreply.github.com>
2024-05-12 01:16:51 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,7 +1,4 @@
 void antiasan(unsigned long addr) {
-    // 將地址右移 3 位，然後加上偏移量
     unsigned long shadow_addr = (addr >> 3) + 0x7fff8000;
-    
-    // 將地址轉換為指針，然後設置其值為 0
     *(char *)shadow_addr = 0;
 }
